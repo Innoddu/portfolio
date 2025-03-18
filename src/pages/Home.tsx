68be8ed0c38582ba93a97ff8b2e58d5d7089e1e2
innoddu <ciui9494@gmail.com>
2025-03-18 11:26:21 -0700

    edit margin home paragraph

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,12 @@ import '../styles/Home.css';
 export function Home() {
     return (
         <Container fluid id="home" className="home min-vh-100" style={{ paddingTop: '80px'}}>
-            <Container className="text-center w-50 d-flex flex-column align-items-center" style={{paddingTop: '100px'}}>
+            <Container className="text-center w-75 d-flex flex-column align-items-center" style={{paddingTop: '100px'}}>
                 <h1 className="display-4 neon-text">HEY, I'M INHO CHOI</h1>
-                <p className=" neon-paragraph fs-5" style={{ lineHeight: '2.0', marginTop: '30px' }}>
-                As a Software Engineer, I love creating something with code. <br />
-                Especially, I'm focusing on developing web and mobile application. <br />
-                I'm so excited to write code that creates what someone needs or even what I need. <br />
+                <p className="neon-paragraph fs-5" style={{ lineHeight: '2.3', marginTop: '30px', }}>
+                As a Software Engineer, I love creating something with code. 
+                Especially, I'm focusing on developing web and mobile application. 
+                I'm so excited to write code that creates what someone needs or even what I need. 
                 </p>
                 <Button
                 href="/Inho_Choi_Resume.pdf"
